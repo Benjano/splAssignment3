eb1048a9e724ea54684a85915b86067f177dba93
Nir Benjano <nir4benjano@gmail.com>
2014-12-09 22:23:57 +0200

    Tests

---
+++ Assignment3/src/Main/RepairMaterial.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class RepairMaterial implements I_RepairMaterial {
+
+	public RepairMaterial(String name, int quantity) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Assignment3/src/Main/RepairTool.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class RepairTool implements I_RepairTool {
+
+	public RepairTool(){
+		
+	}
+
+	public RepairTool(String name, int quantity) {
+		// TODO Auto-generated constructor stub
+	}
+}
