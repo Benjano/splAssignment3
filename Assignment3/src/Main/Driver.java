5d378ccc0405f7cbdce88b3c8ccbebdbe188926e
Nir Benjano <nir4benjano@gmail.com>
2014-12-10 00:12:51 +0200

    Driver

---
+++ Assignment3/src/Main/Driver.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class Driver {
+
+	public Driver(String initialDataSource, String customersGroupsSource, String assetsSource,
+			String assetContentRepairDetailsSource) {
+	}
+
+	public static void main(String[] args) {
+		Driver driver = new Driver("src1", "src1", "src1", "src1");
+		
+	}
+
+}
